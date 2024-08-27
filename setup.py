13f0127071a8fa78c2d014c1c52323d2714ba838
tuanlvo293 <tuanlvo293@gmail.com>
2024-08-27 16:05:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='WLDA',
+    name='WMLDA',
     version='0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
