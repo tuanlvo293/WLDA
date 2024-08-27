cfa42f8ef63484cbabacb21987e00720344cf91c
tuanlvo293 <tuanlvo293@gmail.com>
2024-08-27 16:28:44 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     url='https://github.com/tuanlvo293/WLDA.git',
     author='Tuan L. Vo',
     author_email='tuanlvo293@gmail.com',
-    package_dir={"": "src"},  # Chỉ định thư mục gốc của các package là "src"
-    packages=find_packages(where="src"),  # Tìm các package trong thư mục "src"
+    package_dir={"": "src"},  # Chỉ định thư mục gốc của các packages là "src"
+    packages=find_packages(where="src"),  # Tìm các packages trong thư mục "src"
     keywords='python package',
     python_requires='>=3.6',
     install_requires=[
-        'numpy',  # Thêm các thư viện phụ thuộc nếu có
+        'numpy',
         'scikit-learn',
     ],
 )
