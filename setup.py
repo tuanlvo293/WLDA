514143517dbc352c9897831239589a146a7398e5
tuanlvo293 <tuanlvo293@gmail.com>
2024-08-27 16:37:20 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='WLDA',
@@ -7,8 +7,8 @@ setup(
     url='https://github.com/tuanlvo293/WLDA.git',
     author='Tuan L. Vo',
     author_email='tuanlvo293@gmail.com',
-    package_dir={"": "src"},  # Chỉ định thư mục gốc của các packages là "src"
-    packages=find_packages(where="src"),  # Tìm các packages trong thư mục "src"
+    py_modules=['WLDA'],  # Chỉ định module WLDA.py
+    package_dir={'': 'src'},  # Chỉ định thư mục chứa module là "src"
     keywords='python package',
     python_requires='>=3.6',
     install_requires=[
